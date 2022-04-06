199df89ebc6f7ac7f7e8620cc716f5f13dfa44d3
Pablo Tack <tackpablo@gmail.com>
2022-04-05 19:55:28 -0700

    add general store stats of products and categories to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
     http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
 
   def show
+    @categories = Category.all
+    @category_count = Category.count
+    @product = Product.count
+    
   end
 end
